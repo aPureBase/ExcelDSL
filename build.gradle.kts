841fe1fb8ac6d2f38fdbfa079246dfdd3136ae2a
Jógvan Olsen <jo@jebster.net>
2020-10-17 22:40:55 +0100

    Preparing for bintray publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("com.jfrog.bintray") version "1.8.4"
+    `maven-publish`
 }
 
 group = "com.apurebase"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     jcenter()
@@ -16,6 +20,70 @@ dependencies {
     implementation("org.apache.poi:ooxml-schemas:1.4")
 }
 
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+    from("LICENCE") {
+        into("META-INF")
+    }
+}
+
+
+
+val githubUrl = "https://github.com/aPureBase/ExcelDSL"
+
+publishing {
+    publications {
+        create<MavenPublication>("excel-dsl") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+            artifact(sourcesJar)
+
+            pom {
+                packaging = "jar"
+                name.set(rootProject.name)
+                url.set(githubUrl)
+                scm { url.set(githubUrl) }
+                issueManagement { url.set("$githubUrl/issues") }
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("$githubUrl/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jeggy")
+                        name.set("Jógvan Olsen")
+                    }
+                }
+            }
+        }
+    }
+}
 kotlin {
     explicitApi()
+
+    bintray {
+        user = System.getenv("BINTRAY_USER")
+        key = System.getenv("BINTRAY_KEY")
+
+        publish = true
+        setPublications("excel-dsl")
+        pkg.apply {
+            repo = "apurebase"
+            name = project.name
+            setLicenses("MIT")
+            setLabels("kotlin", "excel", "apache", "poi", "dsl")
+            vcsUrl = githubUrl
+            websiteUrl = githubUrl
+            issueTrackerUrl = "$githubUrl/issues"
+            version.apply {
+                name = "${project.version}"
+                released = "${Date()}"
+            }
+        }
+    }
 }
