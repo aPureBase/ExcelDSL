1140cc85d4157144f776a345918f26ac3291ca85
Jógvan Olsen <jol@apurebase.com>
2022-02-25 15:06:05 +0100

    build.gradle move around

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,25 +35,70 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit_version")
 }
 
+nexusStaging {
+    packageGroup = "com.apurebase"
+    username = sonatypeUsername
+    password = sonatypePassword
+    numberOfRetries = 360 // 1 hour if 10 seconds delay
+    delayBetweenRetriesInMillis = 10000 // 10 seconds
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+    clientTimeout.set(Duration.parse("PT10M")) // 10 minutes
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get())
+}
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    classifier = "javadoc"
+    from(tasks.dokkaHtml)
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = project.name
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(dokkaJar)
+            pom {
+                name.set("ExcelDSL")
+                description.set("A easy to use Kotlin DSL to build Excel documents")
+                organization {
+                    name.set("aPureBase")
+                    url.set("https://apurebase.com/")
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jeggy")
+                        name.set("Jógvan Olsen")
+                        email.set("jol@apurebase.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/aPureBase/ExcelDSL.git")
+                    developerConnection.set("scm:git:https://github.com/aPureBase/ExcelDSL.git")
+                    url.set("https://github.com/aPureBase/ExcelDSL/")
+                    tag.set("HEAD")
+                }
+            }
+        }
+    }
+}
+
 kotlin {
     apply<NexusPublishPlugin>()
-
-    nexusStaging {
-        packageGroup = "com.apurebase"
-        username = sonatypeUsername
-        password = sonatypePassword
-        numberOfRetries = 360 // 1 hour if 10 seconds delay
-        delayBetweenRetriesInMillis = 10000 // 10 seconds
-    }
-
-    nexusPublishing {
-        repositories {
-            sonatype()
-        }
-        clientTimeout.set(Duration.parse("PT10M")) // 10 minutes
-    }
-
-
     explicitApi()
 
     tasks {
@@ -83,58 +128,6 @@ kotlin {
         }
         closeAndReleaseRepository {
             mustRunAfter(subprojects.map { it.tasks.getByName("publishToSonatype") }.toTypedArray())
-        }
-    }
-
-
-
-
-
-    val sourcesJar by tasks.creating(Jar::class) {
-        classifier = "sources"
-        from(sourceSets.main.get())
-    }
-    val dokkaJar by tasks.creating(Jar::class) {
-        group = JavaBasePlugin.DOCUMENTATION_GROUP
-        classifier = "javadoc"
-        from(tasks.dokkaHtml)
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                artifactId = project.name
-                from(components["java"])
-                artifact(sourcesJar)
-                artifact(dokkaJar)
-                pom {
-                    name.set("ExcelDSL")
-                    description.set("A easy to use Kotlin DSL to build Excel documents")
-                    organization {
-                        name.set("aPureBase")
-                        url.set("https://apurebase.com/")
-                    }
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("jeggy")
-                            name.set("Jógvan Olsen")
-                            email.set("jol@apurebase.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/aPureBase/ExcelDSL.git")
-                        developerConnection.set("scm:git:https://github.com/aPureBase/ExcelDSL.git")
-                        url.set("https://github.com/aPureBase/ExcelDSL/")
-                        tag.set("HEAD")
-                    }
-                }
-            }
         }
     }
 }
